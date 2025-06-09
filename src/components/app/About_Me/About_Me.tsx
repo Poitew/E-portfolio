c2c82733efa2633512a090aaaecda9a813f10089
Poitew <tommasoromano123@gmail.com>
2025-06-09 18:35:27 +0200

    fix: h1 not properly displaying on 1080p

--- src/components/app/About_Me/About_Me.tsx
+++ src/components/app/About_Me/About_Me.tsx
@@ -17,19 +17,20 @@ function AboutMe(){
                 <h2 className={styles.h2} >About Me</h2>
 
                 <p className={styles.history}>
-                    Hi! I'm "sickpoitew", a 17-year-old student and IT enthusiast based in Italy with 4 years of 
+                    Hi! I'm sickpoitew, a 18-year-old student and IT enthusiast based in Italy with 4 years of 
                     experience in programming. I'm an aspiring Software Developer and DevOps Engineer constanly trying to improve.
                     <br />
                     <br />
                     My expertise in software development is focused on full-stack development, and as such I have a strong
                     experience with languages and frameworks such as HTML, CSS, JavaScript/TypeScript, ReactJS for front-end, as well as
                     PHP, Node.js and MySQL for the back-end.
-                    I also have a deep interest in a lower level programming, particularly with C++.
+                    I also have a deep interest in a lower level programming, particularly with C++ and Rust.
                     As a DevOps enthusiast I have experience in using Linux-based operating systems, Bash/Python for automating tasks, 
                     Docker for both containerization and deployment, and Jenkins for CI/CD.
                     <br/>
                     <br />
-                    In my free time I love studying anything tech-related, reading books and listening to music.                        I also have a blog where I write my opinions, thoughts, and share tutorials in hope that they will be useful.
+                    In my free time I love studying anything tech-related, reading books and listening to music.
+                    I also have a blog where I share tutorials, and informations for fun.
                 </p>
             </div>
 
